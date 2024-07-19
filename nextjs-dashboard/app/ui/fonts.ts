7f63559ac01c91a3ee1a783430049831b328182c
ckisselburgh <ckisselburgh@abyssalvoid.com>
2024-07-18 20:12:34 -0500

    Add new dashboard components and clean up gitignore
    Implemented new UI components for the side navigation, revenue chart, and navigation links on the dashboard. Also cleaned up the gitignore file by removing unnecessary entries. The new UI components follow latest design guidelines and improve the overall user experience.

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+//
+// Created by chris on 7/18/24.
+// Copyright (c) 2024 Abyssal Void Software. All rights reserved.
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
